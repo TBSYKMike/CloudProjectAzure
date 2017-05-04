b6d166b3b3274a1321d29912e967434e102c9bbe
Mike <Mike@Mike-Dator>
2017-05-04 13:29:30 +0200

    first commit

---
+++ CouldProjectAzureV2/CouldProjectAzureV2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CouldProjectAzureV2.Global" Language="C#" %>
---
+++ CouldProjectAzureV2/CouldProjectAzureV2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CouldProjectAzureV2.Startup))]
+namespace CouldProjectAzureV2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
