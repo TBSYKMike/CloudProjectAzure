ed2c90b46f64d210e900b2bb702f71fdfeeb7e02
Akasuna <kim28138th@gmail.com>
2017-06-07 15:09:21 +0200

    Added coments and cleaned up old comments

--- CouldProjectAzureV2/CouldProjectAzureV2/Default.aspx.cs
+++ CouldProjectAzureV2/CouldProjectAzureV2/Default.aspx.cs
@@ -16,34 +16,7 @@ namespace CouldProjectAzureV2
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //AzureTableConnector azureTableConnector = new AzureTableConnector();
-            //azureTableConnector.RetriveDataFromSensors("people");
-            //create a cookie
-            //    var context = new IdentityDbContext();
-            //  var role = new RoleManager<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>(new Microsoft.AspNet.Identity.EntityFramework.RoleStore<IdentityRole>(dbcontext));
-
-
-            //var users = context.Users.ToList();
-         //   var list = Roles.GetUsersInRole("patient").Select(Membership.GetUser).ToList();
-           // Debug.Write("looksdsdsd:    " + list[0].UserName);
-
-      //  var role = new RoleManager<Microsoft.AspNet.Identity.EntityFramework.IdentityRole>(new Microsoft.AspNet.Identity.EntityFramework.RoleStore<IdentityRole>(dbcontext));
-      //       var context = new ApplicationDbContext();
-      //     var users = from u in context.Users
-      //               where u.Roles.Any(r => r.Role.Name== "patient")
-      //             select u;
-      // Debug.Write("Look!!" )
-
-
-            /*
-
-            var roleUserIdsQuery = from role in context.Roles
-                                   where role.Name == "patient"
-                                   from user in role.Users
-                                   select user.UserId;
-            var users = context.Users.Where(u => roleUserIdsQuery.Contains(u.Id)).ToList();
-            Debug.Write("Look" + users[0].UserName);
-            */
+            
         }
     }
 }
